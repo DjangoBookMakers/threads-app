9a316c4c2648b2e0ac5957367085796e97a77c8e
SeungminAhn <smahn9123@gmail.com>
2025-02-22 16:47:02 +0900

    ✨ add post app for chapter4

---
+++ chapter4/post/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def post_list(request):
+    pass
+
+
+def post_detail(request):
+    pass
+
+
+def post_create(request):
+    pass
+
+
+def post_update(request):
+    pass
+
+
+def post_delete(request):
+    pass
