ec4fdc57ff07a9610f337f39bba250c48b6b757a
SeungminAhn <smahn9123@gmail.com>
2025-02-22 12:23:42 +0900

    ✨ add signup, login, logout, and profile feature for chapter4

--- chapter4/accounts/urls.py
+++ chapter4/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("signup/", views.accounts_signup, name="accounts_signup"),
+    path("profile/", views.accounts_profile, name="accounts_profile"),
     path("login/", views.accounts_login, name="accounts_login"),
     path("logout/", views.accounts_logout, name="accounts_logout"),
-    path("profile/", views.accounts_profile, name="accounts_profile"),
 ]
