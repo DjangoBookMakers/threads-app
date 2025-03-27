a0fb34d2e3d0afa61361a3f621e823df57d27b61
SeungminAhn <smahn9123@gmail.com>
2025-03-27 20:44:39 +0900

    ✨ set user model for chapter4

--- chapter4/accounts/models.py
+++ chapter4/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    profile_image = models.ImageField(
+        upload_to="profile_images/", blank=True, null=True
+    )
+    website = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.username
