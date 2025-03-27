effd6f13ec07602c1fd2d163cd443921bbe0e0e6
SeungminAhn <smahn9123@gmail.com>
2025-03-27 20:11:20 +0900

    🔥 reset chapter4 code

--- chapter4/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("signup/", views.accounts_signup, name="accounts_signup"),
-    path("profile/", views.accounts_profile, name="accounts_profile"),
-    path("login/", views.accounts_login, name="accounts_login"),
-    path("logout/", views.accounts_logout, name="accounts_logout"),
-]
